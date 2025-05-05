142d4f65d7eda91265f0f459beffd39963c58f27
Bas77 <sebastianramli@rocketmail.com>
2025-05-05 23:18:15 +0700

    Changed app name

--- app.config.js
+++ app.config.js
@@ -2,16 +2,19 @@ import 'dotenv/config';
 
 export default {
   expo: {
-    name: "the-knowledge-cache",
+    name: "Knowledge Cache",
     slug: "the-knowledge-cache",
     version: "1.0.0",
     extra: {
       SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
       SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-      SUPABASE_STORAGE_URL: process.env.EXPO_PUBLIC_SUPABASE_STORAGE_URL
+      SUPABASE_STORAGE_URL: process.env.EXPO_PUBLIC_SUPABASE_STORAGE_URL,
+      "eas": {
+        "projectId": "57df02ae-0159-4a62-9cd0-51a0e5e02d38"
+      }
     },
     eas: {
-      projectId: '57df02ae-0159-4a62-9cd0-51a0e5e02d38',
+      projectId: "57df02ae-0159-4a62-9cd0-51a0e5e02d38",
     },
     "android": {
       "package": "com.apaaja.theknowledgecache"
